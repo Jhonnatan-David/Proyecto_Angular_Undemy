957f9f0b8da94a746d4873fe6b91c375324b225c
FamiliaCifuentes <jhonn.deivi@gmail.com>
2022-03-23 22:22:05 -0500

    cargue repositorio Fernando Herrera, para solucionar errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRutingModule } from './app-ruting.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { PagesComponent } from './pages/pages.component';
-
-
 
 
 @NgModule({
@@ -21,19 +21,19 @@ import { PagesComponent } from './pages/pages.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NotpagefoundComponent,
+    NopagefoundComponent,
     DashboardComponent,
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent,
-    PagesComponent
-
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRutingModule
+    AppRoutingModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
